f4d149f7c83c054a36b07e19054219e65d91b3de
Mohamed Ballan <mbh01vb@gmail.com>
2016-02-17 08:48:18 +0200

    again

---
+++ Sample/app/src/main/java/com/mbh/sample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mbh.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    MBLogger logger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
